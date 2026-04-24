b22e60d976708930d288e7ac915a9cc04495ebc1
shin0biX <ujjwalfromkosi@gmail.com>
2026-04-25 02:35:47 +0530

    postgres

--- database.py
+++ database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+print(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+                       connect_args={"sslmode": "require"}
+                       )
 
 SessionLocal = sessionmaker(autocommit=False , autoflush=False , bind=engine)
 
